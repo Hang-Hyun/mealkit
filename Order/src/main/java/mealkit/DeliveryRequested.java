fe26089e6f5f10d255e4f408e5f6026d084312d8
pgh1678 <75401920+pgh1678@users.noreply.github.com>
2021-01-19 04:51:40 +0900

    Add files via upload

---
+++ Order/src/main/java/mealkit/DeliveryRequested.java
@@ -0,0 +1,28 @@
+package mealkit;
+
+public class DeliveryRequested extends AbstractEvent {
+
+
+    private Long id;
+    private Long orderid;
+
+    public DeliveryRequested(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Long orderid) {
+        this.orderid = orderid;
+    }
+}
